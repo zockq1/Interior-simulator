6f201ccc3ff656b109c38291b0c4719787cd9573
zockq1 <zockq1@gmail.com>
2021-11-21 20:16:27 +0900

    control에 벽, 문 설치 방향 변수 및 스위치 함수 추가
    * L mode_1_direction == 1 -> 상
    * L mode_1_direction == 2 -> 하
    * L mode_1_direction == 3 -> 좌
    * L mode_1_direction == 4 -> 우
    SwitchingToUp, Down, Left, Right 함수로 방향 변경

--- Assets/Control&Camera/control.cs
+++ Assets/Control&Camera/control.cs
@@ -8,6 +8,12 @@ using UnityEngine;
 * L mode_1 == 1 -> 바닥 배치 모드
 * L mode_1 == 2 -> 벽 배치 모드
 * L mode_1 == 3 -> 문 배치 모드
+*
+* L mode_1_direction == 1 -> 상
+* L mode_1_direction == 2 -> 하
+* L mode_1_direction == 3 -> 좌
+* L mode_1_direction == 4 -> 우
+*
 *mode == 2 -> 가구 배치 모드
 *mode == 3 -> 1인칭 모드
 *B711205탁재민
@@ -16,12 +22,14 @@ public class control : MonoBehaviour
 {
     public int mode;
     public int mode_1;
+    public int mode_1_direction;
     public Camera camera1, camera2, camera3;
     public bool isdeploying;
     void Start()
     {
         mode = 1;
         mode_1 = 1;
+        mode_1_direction = 1;
         isdeploying = false;
         SwitchingTo1();
     }
@@ -81,6 +89,22 @@ public class control : MonoBehaviour
 
     public void SwitchingToDoor(){
         mode_1 = 3;
+    }
+
+    public void SwitchingToUp(){
+        mode_1_direction = 1;
+    }
+
+    public void SwitchingToDown(){
+        mode_1_direction = 2;
+    }
+
+    public void SwitchingToLeft(){
+        mode_1_direction = 3;
+    }
+
+    public void SwitchingToRight(){
+        mode_1_direction = 4;
     }
 
     public void SwitchingTo2(){
