fc83d6cabd15c9911555986c4a1be517e7335ec4
zockq1 <zockq1@gmail.com>
2021-11-21 16:59:37 +0900

    FirstPersonCamera 이동 구현
    WASD를 이용해 상하좌우 이동, 마우스 휠로 줌인 줌아웃 가능

---
+++ Assets/Control&Camera/ThirdPersonCamera.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+*OrthographicCameraMove
+*WASD 이용해 카메라 이동
+*마우스 휠로 줌인, 줌아웃
+*B711205탁재민
+*/
+public class ThirdPersonCamera : MonoBehaviour
+{
+    private Vector2 anchor, current, pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * 3);
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * 3);
+        }
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * 3);
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * 3);
+        }
+
+        float wheel = Input.GetAxis("Mouse ScrollWheel");
+        this.GetComponent<Camera>().fieldOfView -= wheel * 20;
+
+    }
+}
