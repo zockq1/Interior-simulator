1629b20669833bde5c88b0a38b5e3021c37afa1e
hwanTTing <86951722+hwanTTing@users.noreply.github.com>
2021-11-23 15:33:56 +0900

    CreateFloor수정

--- Assets/floorwalldoorCS/CreateFloor.cs
+++ Assets/floorwalldoorCS/CreateFloor.cs
@@ -2,26 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreateFloor : MonoBehaviour
+public class CreateFloor: MonoBehaviour
 {
-    public GameObject FloorPrefab1;//³ª¹«¹Ù´Ú
-    public GameObject FloorPrefab2;//Å¸ÀÏ¹Ù´Ú
-    public GameObject FloorPrefab3;//´ë¸®¼®¹Ù´Ú
-    public GameObject FloorPrefab4;//½Ã¸àÆ®¹Ù´Ú
+    public GameObject FloorPrefab1;//ë‚˜ë¬´ë°”ë‹¥
+    public GameObject FloorPrefab2;//íƒ€ì¼ë°”ë‹¥
+    public GameObject FloorPrefab3;//ëŒ€ë¦¬ì„ë°”ë‹¥
+    public GameObject FloorPrefab4;//ì‹œë©˜íŠ¸ë°”ë‹¥
     private GameObject temp_floor;
 
-    bool createPossibleFloor = true; //¹Ù´Ú»ı¼º¸ğµå
+    bool createPossibleFloor = true; //ë°”ë‹¥ìƒì„±ëª¨ë“œ
 
-    bool buttonDown = false;//¸¶¿ì½º Å¬¸¯ ÈÄ¿¡ ¶¾ °ÍÀÎÁö °¨Áö.
+    bool buttonDown = false;//ë§ˆìš°ìŠ¤ í´ë¦­ í›„ì— ë—€ ê²ƒì¸ì§€ ê°ì§€.
 
     private Vector3 mouse_Pos;
-    private Vector3 mouse_PosCur;   // ½Ç½Ã°£(ÇöÀç ÇÁ·¹ÀÓ) ¸¶¿ì½º ÁÂÇ¥
-    private Vector3 mouse_startPos; // µå·¡±× ½ÃÀÛ ÁöÁ¡ ¸¶¿ì½º ÁÂÇ¥
-    private Vector3 mouse_finishPos;   // RectÀÇ ÃÖ¼Ò ÁöÁ¡ ÁÂÇ¥
+    private Vector3 mouse_PosCur;   // ì‹¤ì‹œê°„(í˜„ì¬ í”„ë ˆì„) ë§ˆìš°ìŠ¤ ì¢Œí‘œ
+    private Vector3 mouse_startPos; // ë“œë˜ê·¸ ì‹œì‘ ì§€ì  ë§ˆìš°ìŠ¤ ì¢Œí‘œ
+    private Vector3 mouse_finishPos;   // Rectì˜ ìµœì†Œ ì§€ì  ì¢Œí‘œ
     //private Vector3 floorPosition;
-    private Vector3 temp; //À§Ä¡ ÀÓ½Ã ÀúÀå¿ë.
+    private Vector3 temp; //ìœ„ì¹˜ ì„ì‹œ ì €ì¥ìš©.
+    private GridPos gridpos; //GridPosë¡œ ë¶€í„° ê°€ì ¸ì˜¨ ë§ˆìš°ìŠ¤ ì¢Œí‘œ.
 
-    List<GameObject> floorCloneList = new List<GameObject>(); //clone¸®½ºÆ® ÀúÀå.
+    List<GameObject> floorCloneList = new List<GameObject>(); //cloneë¦¬ìŠ¤íŠ¸ ì €ì¥.
     private int cloneNum = 0;
 
 
@@ -35,57 +36,41 @@ public class CreateFloor : MonoBehaviour
     private void Update()
     {
 
-        //UI Å¬¸¯À¸·Î Å¸ÀÏÀÇ Á¾·ù¸¦ ¹Ù²ãÁÜ.
+        //UI í´ë¦­ìœ¼ë¡œ íƒ€ì¼ì˜ ì¢…ë¥˜ë¥¼ ë°”ê¿”ì¤Œ.
         temp_floor = FloorPrefab1;
-
-
-        if(GameObject.Find("control").GetComponent<control>().mode == 1 && GameObject.Find("control").GetComponent<control>().mode_1 == 1){
+        
+        
+        if (GameObject.Find("control").GetComponent<control>().mode == 1 && GameObject.Find("control").GetComponent<control>().mode_1 == 1)
+        {
             createPossibleFloor = true;
         }
-        else{
-             createPossibleFloor = false;
+        else
+        {
+            createPossibleFloor = false;
         }
+        
 
-        //¸¶¿ì½º¸¦ ²ø¾î¼­ Å¸ÀÏ ±î´Â ÀÛ¾÷.
         if (Input.GetMouseButtonDown(0))
         {
             if (createPossibleFloor == true)
             {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-                if (Physics.Raycast(ray, out hit, 10000f))
-                {
-                    mouse_Pos = hit.point;
-                    mouse_Pos.y = transform.position.y;
-                }
-                mouse_startPos.x = (int)mouse_Pos.x + 0.5f;
+                mouse_startPos = GameObject.Find("GridPos").GetComponent<GridPos>().mouse_Pos;
                 mouse_startPos.y = 0.0f;
-                mouse_startPos.z = (int)mouse_Pos.z + 0.5f;
                 buttonDown = true;
             }
         }
 
         if (Input.GetMouseButton(0))
         {
-            //¸¶¿ì½º µå·¡±× Áß
+            //ë§ˆìš°ìŠ¤ ë“œë˜ê·¸ ì¤‘
         }
 
-        if (Input.GetMouseButtonUp(0) && buttonDown == true) //¸¶¿ì½º ¶À
+        if (Input.GetMouseButtonUp(0) && buttonDown == true) //ë§ˆìš°ìŠ¤ ë—Œ
         {
             if (createPossibleFloor == true)
             {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-
-                if (Physics.Raycast(ray, out hit, 10000f))
-                {
-                    mouse_Pos = hit.point;
-                    mouse_Pos.y = transform.position.y;
-                }
-                mouse_finishPos.x = (int)mouse_Pos.x + 0.5f;
+                mouse_finishPos = GameObject.Find("GridPos").GetComponent<GridPos>().mouse_Pos;
                 mouse_finishPos.y = 0.0f;
-                mouse_finishPos.z = (int)mouse_Pos.z + 0.5f;
-
                 temp = mouse_startPos;
                 while (mouse_finishPos.x >= mouse_startPos.x)
                 {
@@ -96,37 +81,22 @@ public class CreateFloor : MonoBehaviour
 
                         GameObject go = Instantiate(temp_floor) as GameObject;
                         go.transform.position = mouse_startPos;
-                        //floorCloneList.Add(go);
-                        //cloneNum += 1;
+
                         mouse_startPos.z += 1.0f;
                     }
                     mouse_startPos.x += 1.0f;
                 }
                 buttonDown = false;
-                //createPossibleFloor = false;
+
             }
 
         }
+     
 
-        //Å¸ÀÏ Delete ·Î »èÁ¦
-        /*
-        if (Input.GetKey(KeyCode.Delete) == true && createPossibleFloor==true)
-        {
-            Debug.Log("Å¸ÀÏ »èÁ¦ ON");
-            if (cloneNum-1 >= 0)
-            {
-                Destroy(floorCloneList[cloneNum-1]);
-                floorCloneList.RemoveAt(cloneNum-1);
-                cloneNum -= 1;
-            }
-
-        }
-        */
-
-        //Å¸ÀÏ ¿À¸¥ÂÊ ¸¶¿ì½º Å¬¸¯À¸·Î »èÁ¦.
+        //íƒ€ì¼ ì˜¤ë¥¸ìª½ ë§ˆìš°ìŠ¤ í´ë¦­ìœ¼ë¡œ ì‚­ì œ.
         if (Input.GetMouseButton(1) && createPossibleFloor == true)
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);    //Ä«¸Ş¶ó¿¡¼­ ·¹ÀÌ¸¦ ½ğ´Ù.
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);    //ì¹´ë©”ë¼ì—ì„œ ë ˆì´ë¥¼ ìœë‹¤.
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit, 30000))
             {
