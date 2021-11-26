e82b7acf9eaadd796b205a722f2ad3aa3c76ba58
hwanTTing <86951722+hwanTTing@users.noreply.github.com>
2021-11-26 18:46:44 +0900

    createFloor수정. 프리팹지정함수 추가.

--- Assets/floorwalldoorCS/CreateFloor.cs
+++ Assets/floorwalldoorCS/CreateFloor.cs
@@ -8,7 +8,7 @@ public class CreateFloor: MonoBehaviour
     public GameObject FloorPrefab2;//타일바닥
     public GameObject FloorPrefab3;//대리석바닥
     public GameObject FloorPrefab4;//시멘트바닥
-    private GameObject temp_floor;
+    private GameObject temp_floor;//설치한 바닥 프리팹 지정변수
 
     bool createPossibleFloor = true; //바닥생성모드
 
@@ -18,8 +18,11 @@ public class CreateFloor: MonoBehaviour
     private Vector3 mouse_PosCur;   // 실시간(현재 프레임) 마우스 좌표
     private Vector3 mouse_startPos; // 드래그 시작 지점 마우스 좌표
     private Vector3 mouse_finishPos;   // Rect의 최소 지점 좌표
-    //private Vector3 floorPosition;
+
+  
     private Vector3 temp; //위치 임시 저장용.
+    private Vector3 tempPos; //이전 마우스 위치 임시 저장.
+    private bool doubleCheck; //같은 위치 중복 생성 방지 변수.
     private GridPos gridpos; //GridPos로 부터 가져온 마우스 좌표.
 
     List<GameObject> floorCloneList = new List<GameObject>(); //clone리스트 저장.
@@ -29,17 +32,33 @@ public class CreateFloor: MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        doubleCheck = true;
         mouse_Pos = transform.position;
+        temp_floor = FloorPrefab1;
     }
     // Update is called once per frame
     
+    //바닥 프리팹 지정함수
+    public void setFloorPrefab1()
+    {
+        temp_floor = FloorPrefab1;
+    }
+    public void setFloorPrefab2()
+    {
+        temp_floor = FloorPrefab2;
+    }
+    public void setFloorPrefab3()
+    {
+        temp_floor = FloorPrefab3;
+    }
+    public void setFloorPrefab4()
+    {
+        temp_floor = FloorPrefab4;
+    }
+
     private void Update()
     {
 
-        //UI 클릭으로 타일의 종류를 바꿔줌.
-        temp_floor = FloorPrefab1;
-        
-        
         if (GameObject.Find("control").GetComponent<control>().mode == 1 && GameObject.Find("control").GetComponent<control>().mode_1 == 1)
         {
             createPossibleFloor = true;
@@ -48,8 +67,28 @@ public class CreateFloor: MonoBehaviour
         {
             createPossibleFloor = false;
         }
-        
 
+
+        if (tempPos != GameObject.Find("GridPos").GetComponent<GridPos>().mouse_Pos)
+        {
+            doubleCheck = true;
+        }
+        
+        //벽 생성과 비슷한 방식으로 바닥 생성 추가
+        /*
+        if (Input.GetMouseButton(0) && createPossibleFloor == true && doubleCheck == true)
+        {
+            gridpos = GameObject.Find("GridPos").GetComponent<GridPos>();
+            tempPos = gridpos.mouse_Pos;
+            mouse_Pos = gridpos.mouse_Pos;
+            mouse_Pos.y = 0.0f;
+            GameObject go = Instantiate(temp_floor) as GameObject;
+            go.transform.position = mouse_Pos;
+            doubleCheck = false;
+
+        }
+        */
+        
         if (Input.GetMouseButtonDown(0))
         {
             if (createPossibleFloor == true)
@@ -91,8 +130,8 @@ public class CreateFloor: MonoBehaviour
             }
 
         }
-     
-
+        
+        
         //타일 오른쪽 마우스 클릭으로 삭제.
         if (Input.GetMouseButton(1) && createPossibleFloor == true)
         {
